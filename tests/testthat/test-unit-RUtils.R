c22c9a2460cccd053221e1c9fd1e14609693155b
Christoph Hösler <christoph.hoesler@gmail.com>
2014-06-23 17:50:20 +0200

    Added test for partition.data.frame

---
+++ tests/testthat/test-unit-RUtils.R
@@ -0,0 +1,28 @@
+# JavaUtils methods
+context("RUtils unit tests")
+
+## jtry
+#######
+
+test_that("partition.data.frame returns a list of data.frames", {
+  # given
+  data(iris)
+
+  # when
+  parts <- partition(iris, 100)
+
+  # then
+  expect_that(parts, is_a("list"))
+  expect_that(all(sapply(parts, is.data.frame)), is_true())
+})
+
+test_that("partition.data.frame contains all rows of the input", {
+  # given
+  data(iris)
+
+  # when
+  parts <- partition(iris, 100)
+
+  # then
+  expect_that(do.call(rbind, parts), equals(iris))
+})
