0aa19ef6bf746e8f41b51d056a47c4cd7f65a53c
Christoph Hösler <christoph.hoesler@gmail.com>
2016-03-03 14:09:12 +0100

    Added comment on DATABASE_TO_UPPER

--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -1,4 +1,5 @@
 DBItest::make_context(JDBC('org.h2.Driver', getOption("h2_jar")), list(url = "jdbc:h2:mem:;DATABASE_TO_UPPER=FALSE", user = 'sa'))
+# DBItest should use dbQuoteIdentifier instead of using DATABASE_TO_UPPER=FALSE
 DBItest::test_result(skip = c(
 	"constructor_strict", "constructor", # too restrictive
 	"get_query_empty_single_column", "get_query_empty_multi_column", # default implementation of dbGetQuery in 0.3.1 returns NULL for empty results
