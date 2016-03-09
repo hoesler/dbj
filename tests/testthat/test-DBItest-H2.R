e704b6f70f994af1ef4f48bc519fd409ee17d620
Christoph Hösler <christoph.hoesler@gmail.com>
2016-03-09 17:49:55 +0100

    Adjusted DBItest config to version 1.1

--- tests/testthat/test-DBItest-H2.R
+++ tests/testthat/test-DBItest-H2.R
@@ -1,7 +1,8 @@
 DBItest::make_context(
 	# DBItest isn't quoting identifiers with dbQuoteIdentifier().
 	# So we have to use experimantal feature DATABASE_TO_UPPER=FALSE instead.
-	JDBC(h2_driver_class(), h2_jar()), list(url = "jdbc:h2:mem:dbi-test;DATABASE_TO_UPPER=FALSE", user = 'sa')
+	dbj::driver(h2_driver_class(), h2_jar()), list(url = "jdbc:h2:mem:dbi-test;DATABASE_TO_UPPER=FALSE", user = 'sa'),
+	tweaks = tweaks(constructor_name = "driver")
 )
 DBItest::test_getting_started(skip = c(
 	"package_name", # too restrictive
@@ -15,10 +16,10 @@ DBItest::test_connection(skip = c(
 	"stress_load_connect_unload" # substitute in make_context does not substitute (h2_jar() is passed on unevaluated)
 ))
 DBItest::test_result(skip = c(
-	"get_query_empty_single_column", "get_query_empty_multi_column", # default implementation of dbGetQuery in 0.3.1 returns NULL for empty results
+	"get_query_empty_single_column", "get_query_empty_multi_column", # default implementation of dbGetQuery in DBI 0.3.1 returns NULL for empty results
 	"stale_result_warning", # Currently I have no simple soulution how to keep track of results
 	"data_logical_int($|_.+)", # I don't understand these tests. Why and when should logicals be ints?
-	"data_date($|_.+)", # date() is undefined in H2.
+	"data_date($|_.+)", # date() function is undefined in H2.
 	"data_time($|_.+)", # why should time be returned as character?
 	"data_timestamp_parens($|_.+)", # datetime is an unsupported function in H2
 	"data_timestamp_utc($|_.+)", # If we store timestamp as SQL TIMESTAMP we have to discard timezone information.
@@ -27,7 +28,8 @@ DBItest::test_result(skip = c(
 	"data_type_connection" # What should be the data type for structure(.(value), class = "unknown1")?
 ))
 DBItest::test_sql(skip = c(
-	"quote_string", "quote_identifier_not_vectorized", # tests default implementation in DBI 0.3.1.
+	"quote_string", # NA is not converted to NULL in DBI 0.3.1.
+	"quote_identifier_not_vectorized", # dbQuoteIdentifier does not use encodeString in DBI 0.3.1
 	"append_table_error", # append to nonexisting table should only fail if create = FALSE
 	"overwrite_table", # similar to truncate
 	"temporary_table", # this is a post 0.3.1 DBI feature
@@ -38,7 +40,6 @@ DBItest::test_sql(skip = c(
 	"roundtrip_timestamp" # TODO: How can I differentiate between POSIXct and POSIXlt?
 ))
 DBItest::test_meta(skip = c(
-	"get_exception", # DBI 0.3.1 tells me to return a list not a character
 	"column_info", # DBI 0.3.1 tells me to return name, field.type, and data.type not "name" and "type"
 	"row_count", # TODO: fix. See comment in function
 	"bind($|_.+)" # dbBind is not defined in DBI 0.3.1
