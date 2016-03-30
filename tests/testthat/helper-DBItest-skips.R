1a2c2de4de4b35e4af1adb755a1b90950c815bf0
Christoph Hösler <christoph.hoesler@gmail.com>
2016-03-30 11:47:36 +0200

    Updated comments

--- tests/testthat/helper-DBItest-skips.R
+++ tests/testthat/helper-DBItest-skips.R
@@ -1,23 +1,22 @@
 # Driver independent DBItest skips
 dbj_skips_global <- c(
-  "package_name",                         # too restrictive
-  "package_dependencies",                 # 'methods' must be listed in 'Depends' for R <= 3.1.1
-  "constructor_strict", "constructor",    # too restrictive
-  "stress_load.*",                        # substitute in make_context does not substitute   
-  "stale_result_warning",                 # Currently I have no simple soulution how to keep track of results
-  "data_logical_int($|_.+)",              # I don't understand these tests. Why and when should logicals be ints?
-  "data_time($|_.+)",                     # why should time be returned as character?
-  "data_timestamp_utc($|_.+)",            # If we store timestamp as SQL TIMESTAMP we have to discard timezone information.
-  "data_64_bit($|_.+)",                   # don't understand the rational behind the test
-  "data_type_connection",                 # What should be the data type for structure(.(value), class = "unknown1")?
-  "append_table_error",                   # append to nonexisting table should only fail if create = FALSE
-  "overwrite_table",                      # similar to truncate
-  "roundtrip_logical_int",                # logicals are currently mapped to BOOLEAN not INT
-  "roundtrip_64_bit",                     # dbWriteTable does not support a field.types argument
+  "package_name",                         # Not an error: too restrictive
+  "constructor_strict", "constructor",    # Not an error: too restrictive
+  "stress_load.*",                        # Not an error: substitute in make_context does not substitute   
+  "stale_result_warning",                 # TODO: Currently I have no simple soulution how to keep track of results
+  "data_logical_int($|_.+)",              # TODO: I don't understand these tests. Why and when should logicals be ints?
+  "data_time($|_.+)",                     # TODO: why should time be returned as character?
+  "data_timestamp_utc($|_.+)",            # TODO: If we store timestamp as SQL TIMESTAMP we have to discard timezone information.
+  "data_64_bit($|_.+)",                   # TODO: don't understand the rational behind this test
+  "data_type_connection",                 # TODO: What should be the data type for structure(.(value), class = "unknown1")?
+  "append_table_error",                   # TODO: append to nonexisting table should only fail if create = FALSE
+  "overwrite_table",                      # TODO: similar to truncate?
+  "roundtrip_logical_int",                # TODO: logicals are currently mapped to BOOLEAN not INT. Change?
+  "roundtrip_64_bit",                     # TODO: dbWriteTable does not support a field.types argument
   "roundtrip_timestamp",                  # TODO: How can I differentiate between POSIXct and POSIXlt?
   "read_only",                            # TODO: Create read only test context for this test
 
-  # The following tests dont't apply to DBI 0.3.1 
+  # The following tests dont't apply to DBI 0.3.1
   "column_info",                          # DBI 0.3.1 tells me to return name, field.type, and data.type not "name" and "type"
   "bind($|_.+)",                          # dbBind is not defined in DBI 0.3.1
   "roundtrip_rownames",                   # this is a post 0.3.1 DBI feature
